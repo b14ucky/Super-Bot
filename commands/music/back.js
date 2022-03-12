cf9115c1633a791b980c36333b7f80522150574b
b14ucky <101473446+b14ucky@users.noreply.github.com>
2022-03-12 19:05:54 +0100

    Create back.js

---
+++ commands/music/back.js
@@ -0,0 +1,44 @@
+const { MessageEmbed } = require('discord.js');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('back')
+		.setDescription('Odtwarza poprzedni utwór!'),
+	async execute(int) {
+        const queue = player.getQueue(int.guild.id);
+
+        const embed = new MessageEmbed();
+
+        embed.setColor('#a83232');
+        embed.setTimestamp();
+        embed.setFooter({
+            text: 'super bot od super mnie',
+            iconURL: int.member.displayAvatarURL({ dynamic: true })
+        });
+
+        if (!queue || !queue.playing) {
+            embed.setTitle('Nic nie jest teraz grane!');
+            return int.reply({
+                embeds: [embed],
+                ephemeral: true
+            });
+        }
+
+        if (!queue.previousTracks[1]) {
+            embed.setTitle('Nic nie było odtwarzane przed tym utworem!');
+            return int.reply({
+                embeds: [embed],
+                ephemeral: true
+            });
+        }
+
+        await queue.back();
+
+        embed.setTitle('Odtwarzam poprzedni utwór!');
+        int.reply({
+            embeds: [embed],
+            ephemeral: true
+        });
+    }
+};
